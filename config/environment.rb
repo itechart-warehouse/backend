02cab23934ddff79fb998afd0a2ce0f160504391
StoreCarCharIsInNoSave <anonvtrede@gmail.com>
2022-02-02 14:43:45 +0300

    US-1.1 Mailer address and password moved to .ENV

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'warehouseitechart@gmail.com',
-  password:             'mekanism225',
+  user_name:            ENV['MAILER_EMAIL_ADDRESS'],
+  password:             ENV['MAILER_EMAIL_PASSWORD'],
   authentication:       :plain,
   enable_starttls_auto: true
 }
