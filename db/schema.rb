c80a010678475420a9168313f64a2c87b9956fb5
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-10 16:17:38 +0300

    TS-2 create ability.rb Add role_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_172541) do
+ActiveRecord::Schema.define(version: 2022_02_10_131422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2022_02_09_172541) do
     t.integer "user_id"
   end
 
+  create_table "user_roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,6 +51,8 @@ ActiveRecord::Schema.define(version: 2022_02_09_172541) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
+    t.integer "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
