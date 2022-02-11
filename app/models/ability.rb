afd5bb7b166d25a6195a91cca6866f0501b5d3b9
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-11 16:39:18 +0300

    TS-2 Added default System Admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,8 +5,8 @@ class Ability
 
   def initialize(current_user)
     role = current_user.role_id
-    if  role==1 # System Admin ability
-       can :manage, :all
+    if    role==1 # System Admin ability
+      can :manage, :all
     elsif role==2 # Company owner ability
       can :read, :user
     elsif role==3 # Company admin ability
