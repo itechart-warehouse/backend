fa9f4cc523502c54f08bc474160c58fb27096815
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-15 14:11:36 +0300

    US-2.2 updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: Blacklist
-  belong_to :company
+  belongs_to :company
 end
