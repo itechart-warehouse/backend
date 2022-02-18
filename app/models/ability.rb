70f2ac24df142205cdcfef5930071a56a4b9e671
Larisa Antokhina <laranto1985@gmail.com>
2022-02-18 12:20:01 +0300

    US-2.8 Rubocoped, added extension to rubocop config

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,19 +5,20 @@ class Ability
 
   def initialize(current_user)
     role = current_user.user_role_id
-    if    role==1 # System Admin ability
+    case role
+    when 1 # System Admin ability
       can :manage, :all
-    elsif role==2 # Company owner ability
+    when 2 # Company owner ability
       can :read, :user
-    elsif role==3 # Company admin ability
+    when 3 # Company admin ability
       can :read, :user
-    elsif role==4 # Warehouse admin ability
+    when 4 # Warehouse admin ability
       can :read, :user
-    elsif role==5 # Dispatcher ability
+    when 5 # Dispatcher ability
       can :read, :user
-    elsif role==6 # Inspector ability
+    when 6 # Inspector ability
       can :read, :user
-    elsif role==7 # Warehouse Manager ability
+    when 7 # Warehouse Manager ability
       can :read, :user
     end
   end
