0acdf875918d2cb7a7d0f2eb55535d8963f71458
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-02-23 21:51:55 +0300

    TS-3 Uncommented parts with CanCan gem. It's time to use it

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -2,8 +2,8 @@
 
 class CompanyController < ApplicationController
   respond_to :json
-  # before_action :access_lvl_helper
-  # load_and_authorize_resource
+  before_action :access_lvl_helper
+  load_and_authorize_resource
 
   def index
     companies = Company.all
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,8 @@
 
 class UserController < ApplicationController
   respond_to :json
+  before_action :access_lvl_helper
+  load_and_authorize_resource
 
   def index
     users = User.all
