810422b6b8fabe82c2b48809b241cc765baca2fd
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-28 09:12:55 +0300

    TS-3 Add jwt token
    Update for US-3.1 Create Warehouse. Changes commented. After merge need update.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,13 +8,13 @@ class Ability
     if @company.active? || @role == 1
       case @role
       when 1 # System Admin ability
-        system_admin_ability()
+        system_admin_ability
       when 2# Company owner ability
-        company_owner_ability()
+        company_owner_ability
       when 3 # Company admin ability
-        company_admin_ability()
+        company_admin_ability
       when 4 # Warehouse admin ability
-        can :read, :user
+        warehouse_admin_ability
       when 5 # Dispatcher ability
         can :read, :user
       when 6 # Inspector ability
@@ -29,6 +29,11 @@ class Ability
     @role = current_user.user_role_id
     @company = Company.find(current_user.company_id)
     @users = @company.users
+    # @warehouses = @company.warehouses
+    # if current_user.warehouse_id != nil
+    #   @warehouse = Warehouse.find(current_user.warehouse_id)
+    #   @warehouse_users = @warehouse.users
+    # end
   end
 
   def system_admin_ability
@@ -42,6 +47,7 @@ class Ability
     can :company_and_roles_list, :all
     can :manage, @company
     can :manage, @users
+    #can :manage, @warehouses
   end
 
   def company_admin_ability
@@ -51,5 +57,15 @@ class Ability
     can :company_and_roles_list, :all
     can :manage, @company
     can :manage, @users
+    #can :manage, @warehouses
+  end
+
+  def warehouse_admin_ability
+    can :index, User
+    can :read, UserRole
+    can :read, Company
+    #can :manage, @warehouse
+    #can :manage, @warehouse_users
+    can
   end
 end
