d36ce2c17c055df6be8d42cb2602b743a703b9cf
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-28 09:55:29 +0300

    TS-3-Add-jwt-token
    Created default admin(seeds.rb)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@ UserRole.create(name: "Warehouse admin")
 UserRole.create(name: "Dispatcher")
 UserRole.create(name: "Inspector")
 UserRole.create(name: "Warehouse Manager")
+Company.create(name: "Appl11e",
+               email: "g1g@g.c",
+               address: "pomos4_ne_pridet",
+               phone: "+73333333333")
+User.create(email: "admin@admin.admin",
+            password: "12345678",
+            password_confirmation: "12345678",
+            first_name: "Admin",
+            last_name: "Adminovi4",
+            user_role_id: 1,
+            company_id: 1,
+            birth_date: "66.66.666",
+            address: "admin room")
