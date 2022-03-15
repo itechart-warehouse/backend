9f9f76b55884663cbaa591b6efd973b9ffea9692
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-03-15 16:52:30 +0300

    US-3.4 Added show action and route

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,6 @@ Rails.application.routes.draw do
   get 'roles', to: 'user_roles#index'
 
   get 'companies/:company_id/warehouses', to: 'warehouse#index'
-
   post 'warehouse/create', to: 'warehouse#create'
+  get 'warehouse/:id', to: 'warehouse#show'
 end
