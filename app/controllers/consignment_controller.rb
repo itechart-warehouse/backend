08fe1ff198072f1e754db12a35e31069e821f4cb
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-03-30 16:22:34 +0300

    updated goods

--- app/controllers/consignment_controller.rb
+++ app/controllers/consignment_controller.rb
@@ -31,4 +31,6 @@ class ConsignmentController < ApplicationController
                                         :passport,
                                         :contractor_name)
   end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_085407) do
+ActiveRecord::Schema.define(version: 2022_03_30_131948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,9 +67,9 @@ ActiveRecord::Schema.define(version: 2022_03_30_085407) do
     t.string "bundle_seria"
     t.string "bundle_number"
     t.string "date"
-    t.string "consignment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "consignment_id"
   end
 
   create_table "product_batches", force: :cascade do |t|
