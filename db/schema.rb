80a87a5a6aa36940c383ef88cc9ee4e3fdee49c3
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-04-06 23:11:57 +0300

    add consignment id to reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_194932) do
+ActiveRecord::Schema.define(version: 2022_04_06_200847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_194932) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "consignment_id"
   end
 
   create_table "sections", force: :cascade do |t|
