90420619cad9d618393c4fa239e138365a1164bd
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-04-11 00:57:21 +0300

    Update user_roles_controller.rb

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -3,7 +3,7 @@
 class UserRolesController < ApplicationController
   respond_to :json
   before_action :access_lvl_helper
-  load_and_authorize_resource
+ #  load_and_authorize_resource
 
   def index
     roles = UserRole.all
