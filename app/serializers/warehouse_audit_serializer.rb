86a7a679196fdfaf0e1760605c872f0a1621f164
Kath Veremeychik <kateiiverem@gmail.com>
2022-05-24 21:05:53 +0300

    feature/LW-8.Display user actions. Add column to table

--- app/serializers/warehouse_audit_serializer.rb
+++ app/serializers/warehouse_audit_serializer.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 class WarehouseAuditSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :username, :company, :data
+  attributes :id, :user_id, :username, :company, :data, :action, :changes, :type
 
   def company
     object.company_id ? Company.find(object.company_id).name : ''
   end
 
   def data
-    object.created_at
+    object.created_at.to_s
+  end
+
+  def changes
+    object.audited_changes
+  end
+
+  def type
+    object.auditable_type
   end
 end
