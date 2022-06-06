8ac0fb3fb5bcf9ff831a25d81a0a1e5e9456627a
Pavel.Trusau <Helbmerliuni@gmail.com>
2022-06-06 15:29:19 +0300

    Feature.lw-7-pagination.fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -38,8 +38,6 @@ class Ability
   end
 
   def standart_ability(current_user)
-    can :page ,User
-    can :page ,Consignment
     can :read , @user
     can :read , @company
     can :read , @warehouse
