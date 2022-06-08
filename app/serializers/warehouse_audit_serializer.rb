1f41a71dd5fa8721106a2a16833cad1fa36aa0fc
Catherine <60444425+MonkeyKingBar-bit@users.noreply.github.com>
2022-06-08 21:21:25 +0300

    fix: change method

--- app/serializers/warehouse_audit_serializer.rb
+++ app/serializers/warehouse_audit_serializer.rb
@@ -4,7 +4,7 @@ class WarehouseAuditSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :username, :company, :data, :action, :changes, :type
 
   def company
-    object.company&.name
+    object.company_id ? Company.find(object.company_id).name : ''
   end
 
   def data
