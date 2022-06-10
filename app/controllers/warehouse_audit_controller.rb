b8f45904b2fb5c9244a5b2818c2860c91b1649e1
Kath Veremeychik <kateiiverem@gmail.com>
2022-06-11 01:31:57 +0300

    feature/LW-8.Display users actions. Add pagination

--- app/controllers/warehouse_audit_controller.rb
+++ app/controllers/warehouse_audit_controller.rb
@@ -2,12 +2,13 @@
 
 class WarehouseAuditController < ApplicationController
   def index
-    statistics = WarehouseAudit.where.not(user_id: nil).alphabetical_sort
+    statistics, meta = paginate_collection(WarehouseAudit.where.not(user_id: nil).alphabetical_sort)
     statistics = statistics.search_name(params[:name]) if params[:name].present?
     statistics = statistics.search_action(params[:actions]) if params[:actions].present?
-    if params[:start_date].present? && params[:end_date].present?
-      statistics = statistics.search_date(params[:start_date], params[:end_date])
-    end
-    render json: statistics
+    statistics = statistics.search_date(params[:start_date], params[:end_date]) if params[:start_date].present? && params[:end_date].present?
+    @serialized_statistics = ActiveModelSerializers::SerializableResource.new(statistics)
+    @serialized_count = meta[:total_count]
+    # render json: statistics
+    render json: { statistic: @serialized_statistics, warehouse_audits_count: @serialized_count }
   end
 end
